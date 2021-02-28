0a2d05b2105b1b6258cc7081638cddeeed8319b4
Audrey-Willis <nnikky15@gmail.com>
2021-02-28 14:28:30 -0500

    changed nav being to small

--- src/components/Header/Nav/Nav.js
+++ src/components/Header/Nav/Nav.js
@@ -6,11 +6,9 @@ import {NavStyle} from "./Nav-Style";
 function Nav(){
     return (
       <NavStyle>
-        <nav>
           <Link to= "/"> Home </Link>
           <Link to= "/Characters">Characters</Link>
           <Link to="/QuoteGenerator">Quote Generator </Link>
-        </nav>
       </NavStyle>
     );
 }
