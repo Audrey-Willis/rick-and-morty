daf8fcffcae3b5df4848b1ced34f36d1716a1018
Audrey-Willis <nnikky15@gmail.com>
2021-02-28 13:10:20 -0500

    filled out logic for quote generator

--- src/components/Quote-Generator/Quote-Generator.js
+++ src/components/Quote-Generator/Quote-Generator.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React,{useState} from "react";
 
 let quotes = [
     {quote:"What, so everyone’s supposed to sleep every single night now? You realize that nighttime makes up half of all time?", speaker:"Rick"}
     ,{quote: "Listen Morty, I hate to break it to you, but what people call love is just a chemical reaction that compels animals to breed. It hits hard, Morty, then it slowly fades, leaving you stranded in a failing marriage. I did it. Your parents are gonna do it. Break the cycle, Morty. Rise above. Focus on science.", speaker: "Rick"}, 
-   {quote: "Listen to me, Morty. I know that new situations can be       intimidating. You're lookin’ around and it’s all scary and different, but y’know … meeting them head-on, charging into ‘em like a bull —  that’s how we grow as people.", speaker: "Rick"}, 
-   {quote: "I'm a scientist; because I invent, transform, create, and destroy for a living, and when I don't like something about the world, I change it.", speaker: "Rick"}, 
+    {quote: "Listen to me, Morty. I know that new situations can be       intimidating. You're lookin’ around and it’s all scary and different, but y’know … meeting them head-on, charging into ‘em like a bull —  that’s how we grow as people.", speaker: "Rick"}, 
+    {quote: "I'm a scientist; because I invent, transform, create, and destroy for a living, and when I don't like something about the world, I change it.", speaker: "Rick"}, 
 	{quote: "Honey, stop raising your father's cholesterol so you can take a hot funeral selfie.", speaker: "Beth"}, 
 	{quote: "Now if you'll excuse me, I've got a quick solo adventure to go on and this one will not be directed by Ron Howard.",speaker: "Rick"},
     {quote: "The universe is basically an animal. It grazes on the ordinary. It creates infinite idiots just to eat them." ,speaker: "Rick"}, 
@@ -22,8 +22,31 @@ let quotes = [
 ;
 
 function QuoteGenerator(){
-    return(
-        <h1>Quote-Generator</h1>
-    )
+    let length = quotes.length;
+    let randomNum = Math.floor(Math.random() * length);
+    let [quote, setQuote] = useState([]);
+    const buttonHandle = (event) => {
+      event.preventDefault();
+      setQuote(quotes[randomNum]);
+    };
+    return (
+      <div>
+        <div className="page-top">
+          <h1>Quote Generator</h1>
+          <p>
+            Our quote generator is easy to use and fun!, just click the button
+            below to see a randomized quote from Rick and Morty. It will soon
+            have you in stitches or make you stop and think.
+          </p>
+        </div>
+        <div className="quoteSection">
+          <div className="insideQuoteSection">
+            <p>{quotes[randomNum].quote}</p>
+            <p> - {quotes[randomNum].speaker}</p>
+            <button onClick={buttonHandle}>New Quote</button>
+          </div>
+        </div>
+      </div>
+    );
 }
 export default QuoteGenerator;
