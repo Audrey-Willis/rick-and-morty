8c1688b8d3873fa71fc45ae2910f3378632eef91
Audrey-Willis <nnikky15@gmail.com>
2021-02-28 13:16:52 -0500

    styled the header using styled-components

---
+++ src/components/Header/Header-Style.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+
+export const HeaderStyle = styled.div`
+	position:relative;
+	text-align:center;
+	display:flex;
+	flex-direction:column;
+	justify:content:center;
+	align-items:center;
+	font-family:cursive;
+	color:black;
+	margin:5%;
+
+img{
+	height: 50vh;
+	width:95%;
+}
+.image-text{
+	position:absolute;
+	display:flex;
+	flex-direction:column;
+	justify-content:center;
+	align-items:center;
+	width:90%;
+}
+`
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Nav from './Nav/Nav';
+import {HeaderStyle} from "./Header-Style";
 
 
 function Header(){
     return (
-      <div>
+      <HeaderStyle>
         <div className="image-text">
           <Nav />
           <h1>Rick and Morty Fan Site!</h1>
           <h2>"wubba lubba dub dub"</h2>
         </div>
         <img src="https://live.staticflickr.com/7622/27982634645_bdbc95feec_b.jpg"alt="Rick and Morty with blurred   background - Menidon" />
-      </div>
+      </HeaderStyle>
     );
 }
 export default Header;
