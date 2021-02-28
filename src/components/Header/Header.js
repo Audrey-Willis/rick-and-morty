39f4eb44f7fa59e785eae9e4073fa197203846b7
Audrey-Willis <nnikky15@gmail.com>
2021-02-28 01:20:36 -0500

    added header html with an image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,11 +3,15 @@ import Nav from './Nav/Nav';
 
 
 function Header(){
-    return(
-<div>
-<h1>Header</h1>
-<Nav/>
-</div>
-    )
+    return (
+      <div>
+        <div className="image-text">
+          <Nav />
+          <h1>Rick and Morty Fan Site!</h1>
+          <h2>"wubba lubba dub dub"</h2>
+        </div>
+        <img src="https://live.staticflickr.com/7622/27982634645_bdbc95feec_b.jpg"alt="Rick and Morty with blurred   background - Menidon" />
+      </div>
+    );
 }
 export default Header;
