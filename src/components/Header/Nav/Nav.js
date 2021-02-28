5b38c5765027f8b0697dbff406d8591cac824434
Audrey-Willis <nnikky15@gmail.com>
2021-02-28 14:58:56 -0500

    fixing deployment issue

--- src/components/Header/Nav/Nav.js
+++ src/components/Header/Nav/Nav.js
@@ -6,7 +6,7 @@ import {NavStyle} from "./Nav-Style";
 function Nav(){
     return (
       <NavStyle>
-          <Link to= "/"> Home </Link>
+          <Link to= "/rick-and-morty/"> Home </Link>
           <Link to= "/Characters">Characters</Link>
           <Link to="/QuoteGenerator">Quote Generator </Link>
       </NavStyle>
